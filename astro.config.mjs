08fd6b84dc3f1e6156e263dfda4bfc46cc720ec2
cheng <565789229@qq.com>
2026-01-28 18:27:56 +0800

    集成cloudglare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare()
 });
